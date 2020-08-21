d02ee2715126f2f862f9caaf0abfb225c798b592
KozaX <55134615+KozaX@users.noreply.github.com>
2020-08-21 14:12:13 +0200

    create model for job applications

---
+++ models/job-application.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+  candidateName: {
+    type: String,
+    required: true,
+    minlength: 3
+  },
+  candidateEmail: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  candidateLocation: {
+    type: String
+  },
+  resumeUpload: {
+    type: String
+  },
+  linkedinUrl: {
+    type: String
+  },
+  githubUrl: {
+    type: String
+  },
+  photo: {
+    type: String
+  }
+});
+
+module.exports = mongoose.model('Job-application', schema);
