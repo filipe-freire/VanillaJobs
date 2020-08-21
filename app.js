2fb6c0976e5c33e28560a49d6951464634abc18d
Filipe Freire <filipefreire@protonmail.com>
2020-08-21 14:42:22 +0100

    Add necessary dependencies

--- app.js
+++ app.js
@@ -7,13 +7,56 @@ const logger = require('morgan');
 const serveFavicon = require('serve-favicon');
 const indexRouter = require('./routes/index');
 
+// Insert Necessary Packages
+const cors = require('cors');
+const dotenv = require('dotenv');
+const expressSession = require('express-session');
+const connectMongo = require('connect-mongo');
+const mongoStore = connectMongo(expressSession);
+const mongoose = require('mongoose');
+
 const app = express();
+
+const deserializeUser = require('./middleware/deserialize-user');
+
+const postRouter = require('./routes/post');
+const authenticationRouter = require('./routes/authentication');
 
 app.use(serveFavicon(join(__dirname, 'public/images', 'favicon.ico')));
 app.use(logger('dev'));
+
+// Middleware
+
+app.use(
+  cors({
+    origin: [process.env.CLIENT_APP_DEV_URL],
+    credentials: true
+  })
+);
 app.use(express.json());
+app.use(
+  expressSession({
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 15 * 24 * 60 * 60 * 1000
+    },
+    store: new mongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 60 * 60
+    })
+  })
+);
+app.use(deserializeUser);
 
 app.use('/', indexRouter);
+
+// Route Handlers
+
+// CHANGE ROUTES TO OUR OWN
+app.use('/job-post', postRouter);
+app.use('/authentication', authenticationRouter);
 
 // Catch missing routes and forward to error handler
 app.use((req, res, next) => {
@@ -21,7 +64,7 @@ app.use((req, res, next) => {
 });
 
 // Catch all error handler
-app.use((error, req, res, next) => {
+app.use((error, req, res) => {
   res.status(error.status || 500);
   res.json({ type: 'error', error: { message: error.message } });
 });
