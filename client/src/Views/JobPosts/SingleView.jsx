49368db0934bb90ef5be660cd792fa2c7e41328c
KozaX <55134615+KozaX@users.noreply.github.com>
2020-08-24 16:08:24 +0200

    add single post view

--- client/src/Views/JobPosts/SingleView.jsx
+++ client/src/Views/JobPosts/SingleView.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import { loadJob } from './../../services/jobPosts';
+
 class SingleView extends Component {
   constructor() {
     super();
@@ -9,12 +11,36 @@ class SingleView extends Component {
     };
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    const id = this.props.match.params.id;
+
+    loadJob(id).then(data => {
+      const { post } = data;
+      console.log(post);
+
+      this.setState({
+        loaded: true,
+        post
+      });
+    });
+  }
 
   render() {
+    const post = this.state.post;
+    console.log(post);
     return (
       <div>
         <h1>Single view</h1>
+        {this.state.loaded && (
+          <>
+            <h4>{post.title}</h4>
+            <p>{post.location}</p>
+            <p>{post.seniority}</p>
+            <p>{post.tasks[0]}</p>
+            <p>{post.requirements[0]}</p>
+            <p>{post.tech}</p>
+          </>
+        )}
       </div>
     );
   }
