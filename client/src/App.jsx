b3c71500b9a30b85b9c2cb462ec1678d7eefceea
Filipe Freire <filipefreire@protonmail.com>
2020-08-27 08:54:14 +0100

    Fix bug on profile view

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -73,12 +73,11 @@ class App extends Component {
 
             <Route path="/public/profile/:id" component={PublicProfile} exact />
             <Route path="/profile/:id/edit" component={ProfileEditView} exact />
-            {this.state.user && (
-              <Route
-                path="/profile/:id"
-                render={props => <Profile {...props} user={this.state.user} exact />} // Anytime we want to pass app.jsx props and more to the component, we use this syntax
-              />
-            )}
+
+            <Route
+              path="/profile/:id"
+              render={props => <Profile {...props} user={this.state.user} exact />} // Anytime we want to pass app.jsx props and more to the component, we use this syntax
+            />
 
             <ProtectedRoute
               path="/authentication/sign-up"
