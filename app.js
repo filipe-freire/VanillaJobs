a13034d1817fda528b24f84148a332f3d201313b
Filipe Freire <filipefreire@protonmail.com>
2020-08-27 11:20:31 +0100

    reverse previous commit

--- app.js
+++ app.js
@@ -45,9 +45,7 @@ app.use(
     resave: true,
     saveUninitialized: false,
     cookie: {
-      maxAge: 15 * 24 * 60 * 60 * 1000,
-      sameSite: 'lax',
-      httpOnly: true
+      maxAge: 15 * 24 * 60 * 60 * 1000
     },
     store: new mongoStore({
       mongooseConnection: mongoose.connection,
